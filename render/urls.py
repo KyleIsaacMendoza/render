df99e02afcea9318001257a55fe8ee48c209c2e0
KyleIsaacMendoza <123342422+KyleIsaacMendoza@users.noreply.github.com>
2023-08-29 01:28:05 +0800

    Add files via upload

---
+++ render/urls.py
@@ -0,0 +1,12 @@
+# render/urls.py
+
+from django.urls import path
+
+from .views import HomePageView
+from .views import AboutPageView
+
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+]
